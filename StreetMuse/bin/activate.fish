17263b34ea1a98c7e5b509a8269ae0ffa3689c9a
Wojtechnology <wswiders@uwaterloo.ca>
2015-02-04 19:21:37 -0500

    LOL

--- StreetMuse/bin/activate.fish
+++ StreetMuse/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/wojtekswiderski/Documents/GitHub/StreetMuse/StreetMuse"
+set -gx VIRTUAL_ENV "/Users/wojtekswiderski/Documents/GitHub/StreetMuse/streetmuse"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- flasky.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/flask')
-def hello_world():
-  return 'This comes from Flask ^_^'
